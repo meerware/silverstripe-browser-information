ad0e4d3f9d6b4f0e3f1d792a089137a6f7282657
helpfulrobot <chris@silverstripe.com>
2015-12-18 09:54:54 +1300

    Converted to PSR-2

--- code/BrowserExtension.php
+++ code/BrowserExtension.php
@@ -4,20 +4,23 @@
  * for a {@link Page_Controller}.
  *
  */
-class BrowserExtension extends Extension {
+class BrowserExtension extends Extension
+{
 
-	/**
-	 * @return Browser is the browser object.
-	 */
-	public function getBrowser() {
-		return Browser::current_browser();
-	}
+    /**
+     * @return Browser is the browser object.
+     */
+    public function getBrowser()
+    {
+        return Browser::current_browser();
+    }
 
-	/**
-	 * Template method for getting the {@link $_SERVER} supplied browser.
-	 * @return Browser
-	 */
-	public function Browser() {
-		return $this->getBrowser();
-	}
+    /**
+     * Template method for getting the {@link $_SERVER} supplied browser.
+     * @return Browser
+     */
+    public function Browser()
+    {
+        return $this->getBrowser();
+    }
 }
